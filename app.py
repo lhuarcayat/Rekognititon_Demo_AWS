a48f420a28828f905b06c9042391ee1c13a016be
Luis Huarcaya <lhuarcayat@uni.pe>
2025-07-20 16:59:30 -0500

    commit25

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = cdk.App()
 RekognitionStack(
     app,
     'RekognitionPocStack',
-  #  env=cdk.Environment(
-   #     account=app.node.try_get_context('account'),
-    #    region=app.node.try_get_context('region') or 'us-east-1'
- #   )
 )
 
 app.synth()
